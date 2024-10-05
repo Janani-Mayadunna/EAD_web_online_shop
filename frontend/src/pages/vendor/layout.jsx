ed7fe2e882478c3a9e0cd98417f882f870a21e73
LochaniRanasinghe <lochaniranasingh2000@gmail.com>
2024-10-05 15:42:56 +0530

    Changes after dev merge

--- frontend/src/pages/vendor/layout.jsx
+++ frontend/src/pages/vendor/layout.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import VendorSidebar from "../../components/VendorSidebar/Sidebar";
+import Sidebar from "../../components/VndorSidebar/Sidebar";
 import "./layout.css";
+import SystemNav from "../../components/AdminNav/AdminNav";
 
 const Layout = ({ children }) => {
   return (
     <div className="mainContainer">
       <div className="sidebar">
-        <VendorSidebar />
+        <Sidebar />
       </div>
-      <div className="contentContainer">{children}</div>
+      <div className="contentContainer">
+        <div className="systemNavBar">
+          <SystemNav />
+        </div>
+        <div className="content">{children}</div>
+      </div>
     </div>
   );
 };
